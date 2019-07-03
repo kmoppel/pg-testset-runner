4a9a3223e9a77174fc06ad09a203ca4d91fc6239
Kaarel Moppel <kaarel.moppel@gmail.com>
2019-07-03 12:24:56 +0300

    Set --random-seed for more predictable pgbench runs

--- testsets/04_s100_c16_bfa_2M/command.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# if you want to customize specific testset's execution create an "override" command.sh file in testsets folder
-
-set -ex
-
-CONNSTR=$(cat connstr.conf)
-/usr/lib/postgresql/11/bin/pgbench "$CONNSTR" -T 1800 -c 16 -M prepared
